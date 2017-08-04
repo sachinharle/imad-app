d91899ac558669b7e0d36e3922b42365df3fb16c
sachinharle <sachinharle@gmail.com>
2017-08-04 15:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+    //Get the name fron request
+    var name =req.params.name;
+    
+    names.push(name);
+    //JSON: Javascript Object Notation
+  res.send(JSON.stringify(names));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
