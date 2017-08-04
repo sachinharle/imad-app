a6283ba3e371dd04691c1c52b5d34e230f92b80a
sachinharle <sachinharle@gmail.com>
2017-08-04 20:30:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,25 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Sachin Harle',
-    heading: 'Article One',
-    date: 'August 1,2017',
-    content: `
-     <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    `
+var articles ={
+    'article-one': {
+        title: 'Article One | Sachin Harle',
+        heading: 'Article One',
+        date: 'August 1,2017',
+        content: `
+         <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        `
+    },
+    'article-two': {
+        title: 'Article Two | Sachin Harle',
+        heading: 'Article Two',
+        date: 'August 15,2017',
+        content: `
+         <p>
+            This is the content of my Second artical.
+        </p>
+        `
+    },
+    'article-three': {
+        title: 'Article Three | Sachin Harle',
+        heading: 'Article Three',
+        date: 'August 28,2017',
+        content: `
+         <p>
+            This is the content of my Third article.
+        </p>
+        `
+    },
 };
+
 function createTemplate(data){
     var title = data.title;
     var heading =data.heading;
+    var date = data.date;
     var content = data.content;
     var htmlTemplate = `
     <html>
@@ -35,7 +59,7 @@ function createTemplate(data){
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
-            
+
             <div class="container">
                 <div>
                     <a href="/">Home</a>
@@ -60,18 +84,27 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-   res.send(createTemplate(article-one));
+app.get('/:articleName', function (req, res){
+    //articleName == article-one
+    //articles[articleName] =={} content object for article-one
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'))
-});
-
-app.get('/article-three', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'))
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
 });
