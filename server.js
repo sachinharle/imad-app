89135e7142a27f6bc1bc14dd9198e27b23d687c5
sachinharle <sachinharle@gmail.com>
2017-08-04 11:28:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,22 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Sachin Harle',
-    heading: 'Article One',
-    date: 'August 1,2017',
-    content: `
-     <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    <p>
-        This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
-    </p>
-    `
+var articles ={
+    'article-one': {
+        title: 'Article One | Sachin Harle',
+        heading: 'Article One',
+        date: 'August 1,2017',
+        content: `
+         <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        <p>
+            This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.This is my artical one.
+        </p>
+        `
+    },
+    'article-two': {
+        title: 'Article Two | Sachin Harle',
+        heading: 'Article Two',
+        date: 'August 15,2017',
+        content: `
+         <p>
+            This is the content of my Second artical.
+        </p>
+        `
+    },
+    'article-three': {
+        title: 'Article Three | Sachin Harle',
+        heading: 'Article Three',
+        date: 'August 28,2017',
+        content: `
+         <p>
+            This is the content of my Third article.
+        </p>
+        `
+    },
 };
+
 function createTemplate(data){
     var title = data.title;
     var heading =data.heading;
@@ -61,17 +84,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res){
+    //articleName == article-one
+    //articles[articleName] =={} content object for article-one
+    var articleName = req.param.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'))
-});
-
-app.get('/article-three', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'))
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
