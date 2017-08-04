04b8841cb312b696e7e7b7e1120afb8d826a67ef
sachinharle <sachinharle@gmail.com>
2017-08-04 12:44:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
+//output on console
 console.log('Loaded!');
+//write an alert
 alert('Hi Sachin, want to proceed');
+//change the text from element "main-text"
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New value';
