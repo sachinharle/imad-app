bd7689cbcdd41d6b93fc64ee2c66e017cf58b8eb
sachinharle <sachinharle@gmail.com>
2017-08-05 10:04:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,20 @@ app.get('/counter', function (req, res){
     res.send(counter.toString());
 });
 
+var names = [];
+/*app.get('/submit-name/:name',function(req, res){
+    var name = req.params.name;
+    names.push(name);
+    //JSON Javascript Object Notation
+    res.send(JSON.stringify(names));
+});*/
+app.get('/submit-name',function(req, res){
+    var name = req.query.name;
+    names.push(name);
+    //JSON Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res){
     //articleName == article-one
     //articles[articleName] =={} content object for article-one
@@ -108,20 +122,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names = [];
-/*app.get('/submit-name/:name',function(req, res){
-    var name = req.params.name;
-    names.push(name);
-    //JSON Javascript Object Notation
-    res.send(JSON.stringify(names));
-});*/
-app.get('/submit-name',function(req, res){
-    var name = req.query.name;
-    names.push(name);
-    //JSON Javascript Object Notation
-    res.send(JSON.stringify(names));
-})
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
