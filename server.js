5de36e2878e471db1d8f29345ac2df98c383cf35
sachinharle <sachinharle@gmail.com>
2017-08-05 10:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,18 +95,18 @@ app.get('/counter', function (req, res){
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req, res){
+/*app.get('/submit-name/:name',function(req, res){
     var name = req.params.name;
     names.push(name);
     //JSON Javascript Object Notation
     res.send(JSON.stringify(names));
-});/*
-app.get('/submit-name',function(req, res){
+});*/
+app.get('/submit-name/:name',function(req, res){
     var name = req.query.name;
     names.push(name);
     //JSON Javascript Object Notation
     res.send(JSON.stringify(names));
-});*/
+});
 
 app.get('/:articleName', function (req, res){
     //articleName == article-one
