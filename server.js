143bf05623c4f14425e4fb0a8759674236bee903
sachinharle <sachinharle@gmail.com>
2017-08-09 23:28:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('gp').Pool;
 
 var config ={
     user: 'sachinharle',
     databse: 'sachinharle',
-    host: 'db.imad.hasura-app.io'
-    port: '5432'
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
     password: process.env.DB_PASSWORD
 };
 
@@ -93,10 +93,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var Pool = require('gp').Pool;
+var Pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request
-    pool.query(select * from test, function(err, result){
+    pool.query('SELECT * FROM test', function(err, result){
        if(err) {
            res.status(500).send(err.toString());
         }
@@ -105,7 +105,7 @@ app.get('/test-db', function (req, res) {
         }
     });
     //return a response
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
 });
 
 app.get('/ui/main.js', function (req, res){
