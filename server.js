7e10bf0348c8b14baabb85f21e9e2f824ce3e2a1
sachinharle <sachinharle@gmail.com>
2017-08-10 18:32:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function createTemplate(data){
                     ${heading}
                 </h3>
                 <div>
-                    ${date}
+                    ${date.DateString}
                 </div>
                 <div>
                     ${content}
