03a36348cf0cbe1f494f11d02468b0fffdd5e387
sachinharle <sachinharle@gmail.com>
2017-08-10 13:31:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,8 @@ app.get('/articles/:articleName', function (req, res){
     var articlename = req.params.articleName;
    pool.query("SELECT * FROM article WHERE title =" + articlename, function(err, result){
       if(err){
-          res.status(500).send(err.toString());
+          res.send(err.toString());
+          //res.status(500).send(err.toString());
       } /*else {
           if(result.rows.length === 0){
               res.status(404).send('Article not found');
