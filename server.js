742abef7f0b7ce67ac327bebd2e42087724fe969
sachinharle <sachinharle@gmail.com>
2017-08-10 13:17:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/articles/:articleName', function (req, res){
     //articleName == article-one
     //articles[articleName] =={} content object for article-one
     var articlename = req.params.articleName;
-    res.send('articlename');
+    res.send("articlename");
 /*   pool.query("SELECT * FROM article WHERE title =" + articlename, function(err, result){
       if(err){
           res.status(500).send(err.toString());
